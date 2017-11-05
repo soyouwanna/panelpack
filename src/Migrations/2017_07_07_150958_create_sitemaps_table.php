c08b80c0347e74b1dd44238fecba6cc6fa0ce35c
carlosmagnum <decowebro@gmail.com>
2017-11-05 13:34:44 +0200

    new file:   src/Migrations/2017_07_07_150958_create_sitemaps_table.php

---
+++ src/Migrations/2017_07_07_150958_create_sitemaps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use DB;
+class CreateSitemapsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 30);
+            $table->timestamps();
+        });
+
+        if( Schema::hasTable('sitemaps')){
+          DB::table('sitemaps')->insert([
+            ['name'=>'sitemap'],
+          ]);
+        };
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sitemaps');
+    }
+}
